8e4bf5ae4e591e0d06f200cd7fc1857148bcc164
K Annabel Bligh <kabligh@gmail.com>
2020-11-12 14:56:58 +0000

    DRY-ed up test code

--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,9 +1,7 @@
 feature "seeing player 2's points" do
   scenario "viewing points" do
     visit '/'
-    fill_in :player_1_name, with: "Annabel"
-    fill_in :player_2_name, with: "Luke"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content("Luke: 100HP")
   end
 end
